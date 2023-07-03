7dc444a60419f04d83f28bd5f132d484261327b5
h-phuc <hphucdev@gmail.com>
2023-07-03 16:15:31 +0700

    #9 CRUD categories blog

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ const { notFound, errHandler } = require('../middlewares/errHandle');
 const userRouter = require('./user');
 const productRotuer = require('./product');
 const productCategoryRotuer = require('./productCategory');
+const blogCategoryRotuer = require('./blogCategory');
 
 const initRouters = (app) => {
   app.use('/api/user', userRouter);
   app.use('/api/product', productRotuer);
   app.use('/api/prodcategory', productCategoryRotuer);
+  app.use('/api/blogcategory', blogCategoryRotuer);
 
   app.use(notFound);
   app.use(errHandler);
